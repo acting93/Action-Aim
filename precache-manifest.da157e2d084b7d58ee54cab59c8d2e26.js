3ebeb5321f5db656e1bd7590a338a5145661d542
acting93 <53194530+acting93@users.noreply.github.com>
2020-09-26 19:47:19 +0200

    Updates

---
+++ precache-manifest.da157e2d084b7d58ee54cab59c8d2e26.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676eef0ab0027d2d5570f1a04950daa0",
+    "url": "/Action-Aim/index.html"
+  },
+  {
+    "revision": "45c36529ff8bdd399b4f",
+    "url": "/Action-Aim/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2f2955226cca51296162",
+    "url": "/Action-Aim/static/css/main.329c68c5.chunk.css"
+  },
+  {
+    "revision": "45c36529ff8bdd399b4f",
+    "url": "/Action-Aim/static/js/2.18bd02cf.chunk.js"
+  },
+  {
+    "revision": "2f2955226cca51296162",
+    "url": "/Action-Aim/static/js/main.8ae86088.chunk.js"
+  },
+  {
+    "revision": "2000e64a09cc8543328a",
+    "url": "/Action-Aim/static/js/runtime-main.68c786cf.js"
+  },
+  {
+    "revision": "44f49a14b83b5e59d98edac1c9a6f7da",
+    "url": "/Action-Aim/static/media/action.44f49a14.png"
+  },
+  {
+    "revision": "aa210a7751adea080cc3afca49c7273b",
+    "url": "/Action-Aim/static/media/bg1.aa210a77.jpg"
+  },
+  {
+    "revision": "9e341949ed4053301c7ed2e77f362eef",
+    "url": "/Action-Aim/static/media/bg3.9e341949.jpg"
+  },
+  {
+    "revision": "b4021e774e9b06aeb4f110f7d2ae1163",
+    "url": "/Action-Aim/static/media/header.b4021e77.jpg"
+  },
+  {
+    "revision": "0b02e7d76ac917adfaadc73b3b20e5b2",
+    "url": "/Action-Aim/static/media/tshirt2.0b02e7d7.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Action-Aim/precache-manifest.da157e2d084b7d58ee54cab59c8d2e26.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Action-Aim/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
