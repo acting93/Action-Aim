10da2428c0e49916369932ad0f3fb076ecafdf0a
acting93 <53194530+acting93@users.noreply.github.com>
2020-10-20 18:20:23 +0200

    add first structure

--- src/SubComponents/Contact.js
+++ src/SubComponents/Contact.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import '../SCSS/Contact.css';
+import {Col} from 'react-bootstrap';
+import Form from './ContactSubComponents/Form';
 
 const Contact = () => {
     return ( 
         <>
-        kontakt
+            <Col xl={10} className='contact-col'>
+                <section className='contact'>
+                    <div className='contact-title'>Formularz kontaktowy</div>
+                    <Form />
+                </section>
+            </Col>
         </>
      );
 }
