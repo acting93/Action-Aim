f35ee2c08450183f8ce5acfc5d36e852e59f8f6d
acting93 <53194530+acting93@users.noreply.github.com>
2020-10-20 19:13:23 +0200

    add information and header to contact

--- src/SubComponents/Contact.js
+++ src/SubComponents/Contact.js
@@ -2,14 +2,27 @@ import React from 'react';
 import '../SCSS/Contact.css';
 import {Col} from 'react-bootstrap';
 import Form from './ContactSubComponents/Form';
+import Logo from '../img/action.png'
 
 const Contact = () => {
     return ( 
         <>
+            <Col xl={12} className='contact-header'>
+                <p>Masz pytanie ? Pisz do nas śmiało !</p>
+            </Col>
             <Col xl={10} className='contact-col'>
                 <section className='contact'>
                     <div className='contact-title'>Formularz kontaktowy</div>
                     <Form />
+                </section>
+                <section className='contact'>
+                    <div className='contact-logo'><img src={Logo} alt=''/></div>
+                    <div className='contact-info'>
+                        <p>Action-Aim</p>
+                        <p>ul. Aleja Grunwaldzka 250 82-300 Elbląg</p>
+                        <p>Tel: 000 000 000</p>
+                        <p>E-mail: actionaim@action.com</p>
+                    </div>
                 </section>
             </Col>
         </>
